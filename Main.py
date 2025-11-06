e3966c62d43272d7e5883c77ea1deb2ff0a964c9
BigZengin <bertil.tartar@gmail.com>
2025-11-06 15:07:52 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -58,6 +58,47 @@ class ExplicitEuler(Solver):
     def step(self, tn, un):
         return un + self.h * self.ivp.f(tn, un)
     
+class Evaluator():
+    def __init__(self, ivp, solverType):
+        self.ivp = ivp
+        self.solverType  = solverType
+    
+    def errvstime(self, N, errType): #errtype is boolean, true absolute.
+        
+        solver = self.solverType(self.ivp, N)
+        solver.integrate()
+        us = solver.us
+        ys = np.array([ivp.exact(t) for t in solver.grid]).T
+        dif =  solver.us - ys
+        if errType:
+            errs = np.array([scipy.linalg.norm(dif[:,i]) for i in range(N+1)])
+        else:
+            errs = np.array([scipy.linalg.norm(dif[:,i])/scipy.linalg.norm(ys[i]) for i in range(N+1)])
+
+        return errs , solver.grid
+    
+    def errvsh(self, Ns, errType):
+        errs = np.zeros(len(Ns))
+        hs = np.zeros(len(Ns))
+        for i in range (len(Ns)):
+            solver = self.solverType(self.ivp, Ns[i])
+            solver.integrate()
+            [error, grid] = self.errvstime(Ns[i], errType)
+            err = error[-1]
+            errs[i] = err
+            hs[i] = Ns[i]/self.ivp.T
+        return errs, hs
+    
+
+
+
+
+
+
+        
+
+
+        
 
 
 #Explicit Euler Scalar plot
@@ -105,3 +146,30 @@ plt.legend()
 plt.show()
 
 
+ys = np.array([ivp.exact(t) for t in solver.grid]).T
+dif =  solver.us - ys
+difnorm= np.array([scipy.linalg.norm(dif[:,i])/scipy.linalg.norm(ys[:, i]) for i in range(solver.N+1)])
+plt.plot(solver.grid, difnorm, label="norm")
+plt.plot(solver.grid ,dif[0,:])
+plt.plot(solver.grid ,dif[1,:])
+plt.legend()
+plt.show()
+
+evaluator = Evaluator(ivp, ExplicitEuler)
+[grid, errs] = evaluator.errvstime(100, True)
+
+# Plot errors with log-scale on the y-axis
+plt.figure()
+plt.semilogy(grid, errs)
+
+# Compute the error at t = T for different h corresponding to N = 2^3, 2^4, ..., 2^10
+Ns = 2**np.arange(3, 11)
+[hs, errs] = evaluator.errvsh(Ns,True)
+
+# Plot these errors in a log-log scale
+plt.figure()
+plt.loglog(hs, errs)
+
+# Plot a comparison line which is O(h)
+plt.loglog(hs, hs * 2 * errs[-1] / hs[-1])
+plt.show()
