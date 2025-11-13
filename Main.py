8b4ffdb988fca1717921f5f48afda0cbf8a3d1ed
BigZengin <bertil.tartar@gmail.com>
2025-11-13 08:25:23 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ class IVP:
 
 
     def f(self,t,y):
-        return NotImplemented("The function f has not been given")
+        raise NotImplementedError("The function f has not been given")
 
 class LTE(IVP):
     def __init__(self, t0, T, y0, A):
@@ -26,29 +26,59 @@ class LTE(IVP):
         return exponential @ self.y0
 
 class Solver:
-    def __init__(self, ivp, N: int):
+    def __init__(self, ivp, N: int, tol=1e-10, adaptive = False):
         self.ivp = ivp
         self.N = N
         self.u = ivp.y0
         self.grid = np.linspace(ivp.t0, ivp.T, N + 1)
         self.h = (ivp.T - ivp.t0) / N
-        self.us = None 
+        self.us = None
+        self.tol = tol
+        self.adaptive = adaptive
 
     def step(self, tn, un):
         raise NotImplementedError("step() not implemented in subclass")
 
     def integrate(self):
-        m = len(self.u) 
-        us = np.zeros((m, self.N + 1))
-        us[:, 0] = self.u
+        if not self.adaptive:
+            # fixed-step branch
+            m = len(self.u)
+            us = np.zeros((m, self.N + 1))
+            us[:, 0] = self.u
+            for i in range(self.N):
+                tn = self.grid[i]
+                un = us[:, i]
+                us[:, i + 1] = self.step(tn, un)
+            self.us = us
+            return self.grid, self.us
 
-        for i in range(self.N):
-            tn = self.grid[i]
-            un = us[:, i]
-            us[:, i + 1] = self.step(tn, un)
+        # adaptive branch
+        T = self.ivp.T
+        tn = self.ivp.t0
+        un = self.u.copy()
+        grid = [tn]
+        us_list = [un.copy()]
 
-        self.us = us
+        k = self.k
+        f0 = self.ivp.f(tn, un)
+        self.h = abs(T - tn) * (self.tol ** (1.0 / k)) / (100 * (1.0 + np.linalg.norm(f0)))
+
+        while tn < T:
+            un = self.step(tn, un)
+            tn += self.h
+            grid.append(tn)
+            us_list.append(un)
+            self.h = self.newstep()
+            if tn + self.h > T:
+                self.h = T - tn
+
+        self.grid = np.array(grid)
+        self.us = np.column_stack(us_list)
         return self.grid, self.us
+
+    def newstep(self):
+        raise NotImplementedError("newstep() not implemented in subclass")
+    
 
 class ExplicitEuler(Solver):
     def __init__(self, ivp, N):
@@ -86,16 +116,4 @@ class Evaluator():
             errs[i] = err
             hs[i] = (self.ivp.T - self.ivp.t0) / Ns[i]  
         return errs, hs
-
-class ImplicitEuler(Solver):
-    def __init__(self, ivp, N):
-         super().__init__(ivp, N)
-        
-    def step(self, tn, un):
-        if hasattr(self.ivp, 'A'):
-            I = np.eye(len(un))
-            A = self.ivp.A
-            u_next = scipy.linalg.solve(I - self.h * A, un)
-        else:
-            raise Exception("You need to provide a matrix A")
-        return u_next
+    
